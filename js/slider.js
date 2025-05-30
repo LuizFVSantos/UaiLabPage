47264e12419732315a46963bd3af91ad450ed5cd
Luiz F V Santos <lfvsantos.bh@gmail.com>
2025-05-30 19:50:38 -0300

    React att

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,6 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const controls = document.querySelectorAll(".control");
 let currentItem = 0;
 const elements = document.querySelectorAll(".elements");
